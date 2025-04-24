379033a68994da0ef0f63558e50bde3e3947da0e
Santiago <santiagodominguezarj@gmail.com>
2025-04-24 20:31:18 -0300

    Creo y copio el punto 3

---
+++ tp2_3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main (){
+    #define N 5
+    #define M 7
+    int i,j;
+    int mt[N][M];
+    for(i = 0;i<N; i++)
+    {
+    for(j = 0;j<M; j++)
+    {
+    mt[i][j]=1+rand()%100;
+    printf("%lf ", mt[i][j]);
+    }
+    printf("\n");
+}
+
+    return 0;
+}

fe202a5c6db754c84cd6ef4997a66e35eda73c5b
Santiago <santiagodominguezarj@gmail.com>
2025-04-24 20:15:59 -0300

    Modifico un poco el tp2_1_1

--- tp2_1_1.c
+++ tp2_1_1.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
 #include<stdlib.h>
-
+#include<time.h>
 // codigo a completar
 #define N 20
 int main(){
     int i;
     double vt[N];
+    srand(time(NULL));
     for(i = 0;i<N; i++)
     {
     vt[i]=1+rand()%100;
-    printf("%f", vt[i]);
+    printf("%.1f", vt[i]);
     }
     return 0;
 }

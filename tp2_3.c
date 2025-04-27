fd2a319363dba74ee138732d3be0eebd43e3edf6
Santiago <santiagodominguezarj@gmail.com>
2025-04-26 23:09:55 -0300

    Punto 3 completo

--- tp2_3.c
+++ tp2_3.c
@@ -1,19 +1,22 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
 int main (){
     #define N 5
     #define M 7
     int i,j;
     int mt[N][M];
+    srand(time(NULL));
+    int *ptr=&mt[0][0];
     for(i = 0;i<N; i++)
     {
-    for(j = 0;j<M; j++)
-    {
-    mt[i][j]=1+rand()%100;
-    printf("%lf ", mt[i][j]);
-    }
+        for(j = 0;j<M; j++)
+        {
+        mt[i][j]=1+rand()%100;
+        printf("%d ", *(ptr+i*M+j));
+        }
     printf("\n");
-}
+    }
 
     return 0;
 }

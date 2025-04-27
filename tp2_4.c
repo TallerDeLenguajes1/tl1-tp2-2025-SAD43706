7b96cab83aad0531ded38e0772fd4cb65895bf35
Santiago <santiagodominguezarj@gmail.com>
2025-04-27 00:21:02 -0300

    Principio del punto 4

---
+++ tp2_4.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+struct compu 
+{
+    int velocidad; // Velocidad de procesamiento en GHz (valor entre 1 y 3)
+    int anio; // Año de fabricación (valor entre 2015 y 2024)
+    int cantidad_nucleos; // Cantidad de núcleos (valor entre 1 y 8)
+    char *tipo_cpu; // Tipo de procesador (apuntador a cadena de caracteres)
+}typedef pc;
+void listarPCs(struct compu pcs[], int cantidad);
+void mostrarMasVieja(struct compu pcs[], int cantidad);
+void mostrarMasVeloz(struct compu pcs[], int cantidad);
+int main (){
+    
+}

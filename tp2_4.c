d15c59333f005d9cebbd2cb8a37f47d6ad95e2a8
Santiago <santiagodominguezarj@gmail.com>
2025-04-27 14:19:26 -0300

    Implementacion de la funcion rand

--- tp2_4.c
+++ tp2_4.c
@@ -12,6 +12,16 @@ void listarPCs(struct compu pcs[], int cantidad);
 void mostrarMasVieja(struct compu pcs[], int cantidad);
 void mostrarMasVeloz(struct compu pcs[], int cantidad);
 int main (){
-    int numpcs=5;
+    int numpcs=5,i,vel,aniox,cant_Nu;
+    srand(time(NULL));
+    char tipos[6][10] = {"Intel", "AMD", "Celeron", "Athlon", "Core","Pentium"};
     pc *pcs=(pc*)malloc(numpcs*sizeof(pc));
+    for ( i = 0; i < 5; i++)
+    {
+        vel=rand() % 3+1;
+        aniox=rand() % 10+2015;
+        cant_Nu=rand () % 8+1;
+        pcs[i]=(pc){vel,aniox,cant_Nu,};
+    }
+    
 }
